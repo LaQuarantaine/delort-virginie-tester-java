3357458dccc0e1447b67d35b1bc166ae9320e849
Virginie Delort <virginie.delort@laposte.net>
2025-02-27 13:37:49 +0100

    Ajout du Mock FareCalculatorService, ajout de l'annotation @DisplayName aux cas de tests, centralisation dans la méthode setUpPerTest des instructions Given, ajout de 3 cas de tests : vérifier si utilisateur occasionnel, verifier que l'utilisateur occasionnel ne bénéficie pas de la remise, verifier que utilisateur régulier bénéficie de la remise

--- src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -7,7 +7,9 @@ import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
+import com.parkit.parkingsystem.service.FareCalculatorService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -29,9 +31,12 @@ public class ParkingServiceTest {
     private static ParkingSpotDAO parkingSpotDAO;
     @Mock
     private static TicketDAO ticketDAO;
+    @Mock
+    private FareCalculatorService fareCalculatorService;
 
 
     @BeforeEach 
+    @DisplayName("Préparation du test : Initialisation des mocks et des objets nécessaires")
     private void setUpPerTest() {
         try {
             when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
@@ -41,22 +46,61 @@ public class ParkingServiceTest {
             ticket.setInTime(new Date(System.currentTimeMillis() - (60*60*1000)));
             ticket.setParkingSpot(parkingSpot);
             ticket.setVehicleRegNumber("ABCDEF");
+            
             when(ticketDAO.getTicket(anyString())).thenReturn(ticket);
             when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
-
             when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
+            when(ticketDAO.getNbTicket(anyString())).thenReturn(1);
 
-            parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+            parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO, fareCalculatorService);
         } catch (Exception e) {
             e.printStackTrace();
             throw  new RuntimeException("Failed to set up test mock objects");
         }
     }
  
-    @Test
-    public void processExitingVehicleTest(){
+    @Test 
+    @DisplayName("Mise à jour de la disponibilité du parking")
+    public void processExitingVehicleUpdateParking(){
         parkingService.processExitingVehicle();
         verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
     }
     
+    @Test
+    @DisplayName("Vérifier si utilisateur régulier")
+    public void processExitingVehicle_ShouldCheckUserRegular(){
+        // Given
+	    when(ticketDAO.getNbTicket("ABCDEF")).thenReturn(2); 
+        
+        // When
+    	parkingService.processExitingVehicle();
+    	
+    	// Then
+        verify(ticketDAO, Mockito.times(1)).getNbTicket("ABCDEF");
+    }
+    
+    @Test
+    @DisplayName("Vérifier que l'utilisateur occasionnel ne bénéficie pas de la remise")
+    public void processExitingVehicle_ShouldNotApplyDiscount_IfUserNotRegular() {
+    	// Given
+		
+	    // When
+  	    parkingService.processExitingVehicle();
+	    
+	    // Then
+	    verify(fareCalculatorService, Mockito.times(1)).calculateFare(any(Ticket.class));
+    }   
+    
+    @Test
+    @DisplayName("Vérifier que l'utilisateur régulier bénéficie de la remise")
+    public void processExitingVehicle_ShouldApplyDiscount_IfUserRegular() {
+    	// Given
+        when(ticketDAO.getNbTicket(anyString())).thenReturn(2);
+	    
+	    // When
+	    parkingService.processExitingVehicle();
+	    
+	    // Then
+	    verify(fareCalculatorService, Mockito.times(1)).calculateFare(any(Ticket.class), eq(true));
+    }   
 }
