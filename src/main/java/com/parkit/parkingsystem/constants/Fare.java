121a9fcce6d95e072637d4fe95d54a8aa05a294e
Virginie Delort <virginie.delort@laposte.net>
2025-03-17 17:15:58 +0100

    Ajout de 2 constantes métier : coef de remise et durée gratuite

--- src/main/java/com/parkit/parkingsystem/constants/Fare.java
+++ src/main/java/com/parkit/parkingsystem/constants/Fare.java
@@ -3,4 +3,8 @@ package com.parkit.parkingsystem.constants;
 public class Fare {
     public static final double BIKE_RATE_PER_HOUR = 1.0;
     public static final double CAR_RATE_PER_HOUR = 1.5;
+    
+  //ajout constantes métier
+  	public static final double COEF_DISCOUNT = 0.95;	// soit 5% de remise pour les utilisateurs réguliers
+  	public static final int FREE_PARKING_DURATION = 30 * 60 * 1000; // soit 30 min en millissecondes, durée de stationnement gratuite
 }
